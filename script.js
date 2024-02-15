f606b9c67c5a367fef312b63b0c8230f134b3320
shahzad akram <alyakram786@gmail.com>
2024-02-15 15:31:32 +0500

    inserting arrays

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 let data = [{
+        img: "./Assets/images/webDeveloper.jpeg",
         name: "Susan Smith",
         profession: "WEB DEVELOPER",
         text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
     },
     {
+        img: "././Assets/images/webdisigner.jpeg",
         name: "Anna Johnson",
         profession: "WEB DESIGNER",
         text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
     },
     {
+        img: "././Assets/images/intern.jpeg",
         name: "Peter Jones",
         profession: "INTERN",
         text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
     },
     {
+        img: "./Assets/images/theboos.jpeg",
         name: "Bill Anderson",
         profession: "THE BOSS",
         text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
     }
 ]
+
+
+
+// here are the from html
+const DocCeo = document.querySelector(".main");
