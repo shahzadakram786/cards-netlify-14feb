abeb04a6d61061ec58ef6e9af6593d1dadf11b26
shahzad akram <alyakram786@gmail.com>
2024-02-15 15:14:26 +0500

    starting working on cards prj

---
+++ script.js
@@ -0,0 +1,21 @@
+let data = [{
+        name: "Susan Smith",
+        profession: "WEB DEVELOPER",
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        name: "Anna Johnson",
+        profession: "WEB DESIGNER",
+        text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+    {
+        name: "Peter Jones",
+        profession: "INTERN",
+        text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    },
+    {
+        name: "Bill Anderson",
+        profession: "THE BOSS",
+        text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    }
+]
