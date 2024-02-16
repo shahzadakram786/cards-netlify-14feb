f5e1ac965ac8bf0ccd4e4653c46ff1e59c718b58
shahzad akram <alyakram786@gmail.com>
2024-02-16 13:56:45 +0500

    starting work on cards

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// const data = [{
+//         img: "./Assets/images/webDeveloper.jpeg",
+//         name: "Susan Smith",
+//         profession: "WEB DEVELOPER",
+//         text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+//     },
+//     {
+//         img: "./Assets/images/Webdisigner.jpeg",
+//         name: "Anna Johnson",
+//         profession: "WEB DESIGNER",
+//         text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+//     },
+//     {
+//         img: "./Assets/images/intern.jpeg",
+//         name: "Peter Jones",
+//         profession: "INTERN",
+//         text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+//     },
+//     {
+//         img: "./Assets/images/theboos.jpeg",
+//         name: "Bill Anderson",
+//         profession: "THE BOSS",
+//         text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+//     }
+// ]
+
+
+
+// // here are the from html
+// const DocCeo = document.querySelector(".main");
+// let count = 0;
+
+// for (let i = 0; i < data.length; i++) {
+//     DocCeo.innerHTML +=
+//         `        <div class ="InsCard">
+
+//         <div class ="images"> 
+//         <img src="${data[i].img}" alt="hel" id="img">
+//         </div>
+
+//        <div class = "textArea">
+//         <h2>${data[i].name}</h2>
+//         <h3>${data[i].profession}</h3>
+//         <p>${data[i].text}</p>
+//         </div>
+
+//         </div>
+//     `
+
+// }
+
+
+
+
 const data = [{
         img: "./Assets/images/webDeveloper.jpeg",
         name: "Susan Smith",
@@ -5,13 +59,13 @@ const data = [{
         text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
     },
     {
-        img: "././Assets/images/webdisigner.jpeg",
+        img: "./Assets/images/Webdisigner.jpeg",
         name: "Anna Johnson",
         profession: "WEB DESIGNER",
         text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
     },
     {
-        img: "././Assets/images/intern.jpeg",
+        img: "./Assets/images/intern.jpeg",
         name: "Peter Jones",
         profession: "INTERN",
         text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
@@ -22,18 +76,14 @@ const data = [{
         profession: "THE BOSS",
         text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
     }
-]
+];
 
-
-
-// here are the from html
-const DocCeo = document.querySelector(".main");
+const docCeo = document.querySelector(".main");
 let count = 0;
 
 for (let i = 0; i < data.length; i++) {
-    DocCeo.innerHTML +=
-        `
-        <div class ="InsCard">
+    docCeo.innerHTML +=
+        `        <div class ="InsCard">
 
         <div class ="images"> 
         <img src="${data[i].img}" alt="hel" id="img">
@@ -44,8 +94,39 @@ for (let i = 0; i < data.length; i++) {
         <h3>${data[i].profession}</h3>
         <p>${data[i].text}</p>
         </div>
-        
-        </div>
-    `
 
+        </div>`
+}
+
+function showSlide(n) {
+    count += n;
+    if (count < 0) {
+        count = data.length - 1;
+    } else if (count >= data.length) {
+        count = 0;
+    }
+    displaySlide();
+}
+
+function displaySlide() {
+    const slides = document.querySelectorAll(".InsCard");
+    slides.forEach((slide, index) => {
+        if (index === count) {
+            slide.style.display = "block";
+        } else {
+            slide.style.display = "none";
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    displaySlide();
+});
+
+function prevSlide() {
+    showSlide(-1);
+}
+
+function nextSlide() {
+    showSlide(1);
 }
